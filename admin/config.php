e54a61f14076989566b9cae1313580b795fa0c9a
Lord Sanik <sispow26@gamil.com>
2020-06-29 13:27:18 -0500

    Creacion de las carpetas admin, css, views, iamgenes, imagens a subir. Creacion de los archivos index.php, index.view.php, config.php, estilos.php.Se agrragaron a la carpeta imagens los archivos 1.png, 2.png, 3.png, 4.png

---
+++ admin/config.php
@@ -0,0 +1,23 @@
+<?php 
+
+define('RUTA','http://localhost/curso_php/practicas/blog/');
+
+$bd_config = array(
+    'basedatos' => 'curso_blog',
+    'usuario' => 'root',
+    'pass' => ''
+
+);
+
+$blog_config = array(
+    'post-por-pagina' => '2',
+    'carpeta_imagenes' => 'imagenes/'
+); 
+
+$blog_admin = array(
+    'usuario' => 'Eduardo',
+    'password' => '123'
+
+); 
+
+?>
